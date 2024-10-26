63b91fe2a63fbb0a97b3c89073ae13d306226181
Adam Swanson <adam.swanson.98@gmail.com>
2024-10-26 10:40:25 -0400

    Add time command

---
+++ AdamsMultiTool.CLI/Commands/TimeCommand.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace AdamsMultiTool.CLI.Commands;
+
+public class TimeCommand(TimeProvider timeProvider) : Command<TimeCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+    }
+
+    public override int Execute(CommandContext context, Settings settings)
+    {
+        var currentTime = timeProvider.GetLocalNow();
+        
+        AnsiConsole.WriteLine(currentTime.ToString(CultureInfo.InvariantCulture));
+        return 0;
+    }
+}
--- AdamsMultiTool.CLI/Program.cs
+++ AdamsMultiTool.CLI/Program.cs
@@ -10,13 +10,26 @@ internal static class Program
     private static async Task Main(string[] args) =>
         await BuildApp(GetServices()).RunAsync(args);
 
-    private static ServiceCollection GetServices() => [];
+    private static ServiceCollection GetServices()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton(TimeProvider.System);
+        
+        return services;
+    }
 
     private static CommandApp BuildApp(IServiceCollection services)
     {
         var app = new CommandApp(new TypeRegistrar(services));
 
-        app.Configure(config => config.AddCommand<HelloCommand>("hello"));
+        app.Configure(config =>
+        {
+            config.AddCommand<HelloCommand>("hello");
+            config.AddCommand<TimeCommand>("time")
+                .WithAlias("now")
+                .WithDescription("Print the current time.");
+        });
 
         return app;
     }
